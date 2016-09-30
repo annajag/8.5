19545fbc54c4d6edc3e47ab68dc81068944fb852
annajag <annajagiela@MacBook-Air-Anna.local>
2016-09-30 14:16:45 +0200

    8.5

---
+++ scripts.js
@@ -0,0 +1,19 @@
+a = 5;
+b = 10;
+value = (a*a) + (2 * a * b) - (b*b);
+
+console.log(value);
+
+if (value > 0) {
+	console.log("wartosc dodatnia");
+
+}
+else if (value === 0) { 
+	console.log("wartosc 0");
+
+}
+else { 
+	console.log ("wartosc ujemna");
+    
+}
+console.log(value);
